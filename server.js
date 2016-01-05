1ad106057436714a19f3108922d1081e62210b4d
Rob <robert.erez@gmail.com>
2016-01-05 17:14:52 +1000

    Serve static content using expressjs

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('G\'Day World');
+'use strict';
+
+var express = require('express');
+
+var app = express();
+app.use(express.static('public'));
+
+var server = app.listen(8081, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
