4a3c09b10fb0e44ac188fab99dd3cd4c056ae91b
Rob <robert.erez@gmail.com>
2016-01-06 10:32:06 +1000

    Build with gulp

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    jsmin = require('gulp-jsmin'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean');
+
+gulp.task('less', function() {
+  return gulp.src('./src/less/**/*.less')
+      .pipe(less())
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(['./src/js/vendor/**/*.js', './src/js/app/**/*.js'])
+      .pipe(concat('script.js'))
+      .pipe(jsmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('build', ['less', 'scripts']);
+
+gulp.task('default', ['less', 'scripts'], function() {
+  gulp.watch('./src/less/**/*.less', ['less']);
+  gulp.watch(['./src/js/vendor/**/*.js', './src/js/app/**/*.js'], ['scripts']);
+});
+
+gulp.task('clean-public', function () {
+  return gulp.src(['./public/js', './public/css'], {read: false})
+      .pipe(clean());
+});
